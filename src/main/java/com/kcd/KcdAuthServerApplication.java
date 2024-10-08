37d67097360e2eef9d6974fa9dba3a5f89ee66b0
UD <ckyou@konkuk.ac.kr>
2024-10-08 16:45:58 +0900

    kcdAuthServer 인증 인가 서버 구현

---
+++ src/main/java/com/kcd/KcdAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.kcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcdAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcdAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcd/KcdAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcdAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
